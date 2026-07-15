17266d497da7eff3a2aef971055b182857da29ce
cristhian <cristhianchulca49@gmail.com>
2026-07-15 13:57:35 +0200

    feat(infrastructure) Add TrackEntity class for track persistence in PostgreSQL

---
+++ src/main/java/org/example/infrastructure/persistence/TrackEntity.java
@@ -0,0 +1,33 @@
+package org.example.infrastructure.persistence;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.domain.model.Status;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tracks")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TrackEntity {
+        @Id
+        @Column(name = "id", nullable = false, updatable = false, unique = true)
+        private UUID id;
+
+        @Column(name = "file_name", length = 255, updatable = false)
+        private String fileName;
+
+        @Column(name = "file_path", nullable = false, updatable = false)
+        private String filePath;
+
+        @Column(name = "status", nullable = false)
+        @Enumerated(EnumType.STRING)
+        private Status status;
+
+        public void updateStatus(Status status) {
+                this.status = status;
+        }
+}
