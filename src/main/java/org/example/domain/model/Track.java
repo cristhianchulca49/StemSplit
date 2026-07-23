3a03b1c6a9001b4b68bd0f6356a8ded3c4f572f4
cristhian <cristhianchulca49@gmail.com>
2026-07-23 19:10:00 +0200

    feat(domain) Add create method to Track for simplified instantiation

--- src/main/java/org/example/domain/model/Track.java
+++ src/main/java/org/example/domain/model/Track.java
@@ -15,6 +15,15 @@ public class Track {
         this.status = status;
     }
 
+    public static Track create (UUID trackId, FileName fileName, FilePath filePath){
+        return new Track(
+                trackId,
+                fileName,
+                filePath,
+                Status.PENDING
+        );
+    }
+
     public static Track reconstitute(UUID id, FileName fileName, FilePath filePath, Status status) {
         return new Track(id, fileName, filePath, status);
     }
