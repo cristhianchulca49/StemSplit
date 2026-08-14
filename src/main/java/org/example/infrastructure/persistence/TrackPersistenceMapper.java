6b2473ae7b7044f57bb8e4aa938e501197dc7e39
cristhian <cristhianchulca49@gmail.com>
2026-08-14 11:25:36 +0200

    feat(domain) Update StemEntity and TrackEntity for improved stem management

--- src/main/java/org/example/infrastructure/persistence/TrackPersistenceMapper.java
+++ src/main/java/org/example/infrastructure/persistence/TrackPersistenceMapper.java
@@ -1,12 +1,19 @@
 package org.example.infrastructure.persistence;
 
+import lombok.AllArgsConstructor;
 import org.example.domain.model.FileName;
 import org.example.domain.model.FilePath;
+import org.example.domain.model.Stem;
 import org.example.domain.model.Track;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Component
+@AllArgsConstructor
 public class TrackPersistenceMapper {
+    private StemPersistenceMapper stemMapper;
 
     public TrackEntity toEntity(Track track) {
         return TrackEntity.builder()
@@ -18,12 +25,17 @@ public class TrackPersistenceMapper {
     }
 
     public Track toDomain(TrackEntity trackEntity) {
+        Set<Stem> stems = Optional.ofNullable(trackEntity.getStems())
+                .orElseGet(Collections::emptySet)
+                .stream()
+                .map(stemMapper::toDomain)
+                .collect(Collectors.toSet());
         return Track.reconstitute(
                 trackEntity.getTrackId(),
                 FileName.of(trackEntity.getFileName()),
                 FilePath.reconstitute(trackEntity.getFilePath()),
                 trackEntity.getStatus(),
-                trackEntity.getStems()
+                stems
         );
     }
 }
