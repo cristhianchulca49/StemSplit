569fd9ea16e5a279154f0aa47730c4643d3b0efc
cristhian <cristhianchulca49@gmail.com>
2026-08-14 11:25:32 +0200

    feat(domain) Update StemEntity and TrackEntity for improved stem management

--- src/main/java/org/example/infrastructure/persistence/TrackEntity.java
+++ src/main/java/org/example/infrastructure/persistence/TrackEntity.java
@@ -3,7 +3,6 @@ package org.example.infrastructure.persistence;
 import jakarta.persistence.*;
 import lombok.*;
 import org.example.domain.model.Status;
-import org.example.domain.model.Stem;
 
 import java.util.Set;
 import java.util.UUID;
@@ -15,25 +14,30 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TrackEntity {
-        @Id
-        @Column(name = "track_id", nullable = false, updatable = false, unique = true)
-        private UUID trackId;
+    @Id
+    @Column(name = "track_id", nullable = false, updatable = false, unique = true)
+    private UUID trackId;
 
-        @Column(name = "file_name", length = 255, updatable = false)
-        private String fileName;
+    @Column(name = "file_name", length = 255, updatable = false)
+    private String fileName;
 
-        @Column(name = "file_path", nullable = false, updatable = false)
-        private String filePath;
+    @Column(name = "file_path", nullable = false, updatable = false)
+    private String filePath;
 
-        @Column(name = "status", nullable = false)
-        @Enumerated(EnumType.STRING)
-        private Status status;
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
-        @Column(name = "steams")
-        @Enumerated(EnumType.STRING)
-        @OneToMany(mappedBy = "track", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-        private Set<Stem> stems;
-        public void updateStatus(Status status) {
-                this.status = status;
-        }
+    @Enumerated(EnumType.STRING)
+    @OneToMany(mappedBy = "track", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<StemEntity> stems;
+
+    public void updateStatus(Status status) {
+        this.status = status;
+    }
+
+    public void addStem(StemEntity stem) {
+        this.stems.add(stem);
+        stem.setTrack(this);
+    }
 }
